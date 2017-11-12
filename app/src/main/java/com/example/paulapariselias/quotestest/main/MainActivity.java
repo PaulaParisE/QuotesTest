c1ce64cd660bd384983606a0d297f27f16d392ad
paulapariselias <paulapariselias@gmail.com>
2017-11-11 23:02:50 -0300

    ADD: third commit.

--- app/src/main/java/com/example/paulapariselias/quotestest/main/MainActivity.java
+++ app/src/main/java/com/example/paulapariselias/quotestest/main/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
+
             
             @Override
             public void onClick(View view) {
